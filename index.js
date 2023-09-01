57bc4c5b6d6bed6da956ebfe6b7c05eeefbc6617
saravanaakumar99 <saravanakumar.vcb@gmail.com>
2023-09-01 22:29:10 +0530

    custom component

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
+function Page(){
+    return(
 
-const page=(
-    <div>
-        <img src="pngegg (2).png" width="40px"/>
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-)
+        <div>
+           <ol>
+               <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+               <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+        </div>
+    )
+}
 
-ReactDOM.render(page,document.getElementById("root"))
+ReactDOM.render(<Page />,document.getElementById("root"))
