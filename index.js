1b5ba63c6d13dbdea4bdfef3d46721ea1c1ba325
saravanaakumar99 <saravanakumar.vcb@gmail.com>
2023-09-01 21:23:23 +0530

    practice

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+
 const page=(
     <div>
-        <h1>my awesome website in react</h1>
-        <h3>i love react</h3>
-        <ol>
-            <li>composable</li>
-            <li>declarative</li>
-            <li>hirable skill</li>
-        </ol>
-        
+        <img src="pngegg (2).png" width="40px"/>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
     </div>
 )
+
 ReactDOM.render(page,document.getElementById("root"))
-document.getElementById("root").append(JSON.stringify(page))
