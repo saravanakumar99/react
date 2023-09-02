07b7251a48ba36bd2c608a494ddfdc1030940f16
saravanaakumar99 <saravanakumar.vcb@gmail.com>
2023-09-02 11:18:07 +0530

    custom component part2

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function Page(){
     return(
 
-        <div>
-           <ol>
-               <li>It's a popular library, so I'll be 
+       <div>
+            <header>
+                <nav>
+                    <img src="pngegg (2).png" width="40px" />
+                </nav>
+            </header>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
                 able to fit in with the cool kids!</li>
-               <li>I'm more likely to get a job as a developer
+                <li>I'm more likely to get a job as a developer
                 if I know React</li>
             </ol>
+            <footer>
+                <small>© 2021 Ziroll development. All rights reserved.</small>
+            </footer>
         </div>
     )
 }
