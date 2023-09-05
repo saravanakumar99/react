18ff76a6a36ce44075ab909bd4a4be679f058983
saravanaakumar99 <saravanakumar.vcb@gmail.com>
2023-09-05 21:22:17 +0530

    project-01

---
+++ playbround.js
@@ -0,0 +1,10 @@
+function Img(){
+    return(
+        <div>
+            <h1>hello</h1>
+            <img src="{logo}" alt="logo"/>
+        </div>
+    )
+}
+
+ReactDOM.render(<Img />,document.getElementById("root"))
