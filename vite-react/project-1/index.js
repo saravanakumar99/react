b6d06b4b24283e0c1656c63f0fc9a3cd36c158c3
saravanaakumar99 <saravanakumar.vcb@gmail.com>
2023-09-10 11:46:57 +0530

    props-basics

--- vite-react/project-1/index.js
+++ vite-react/project-1/index.js
@@ -1,4 +1,20 @@
 import React from "react";
 import ReactDOM  from "react";
-import App from "./App";
+
+function App() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    
+    if (hours < 12) {
+        timeOfDay = "morning"
+    } else if (hours >= 12 && hours < 13) {
+        timeOfDay = "afternoon"
+    } else {
+        timeOfDay = "night"
+    }
+    return (
+        <h1>Good {timeOfDay}!</h1>
+    )
+}
 ReactDOM.render(<App />,document.getElementById("root"))
