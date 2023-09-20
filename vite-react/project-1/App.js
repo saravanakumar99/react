f4761e0f85a6c6ac660d94a8e04a093e587eddce
saravanaakumar99 <saravanakumar.vcb@gmail.com>
2023-09-20 22:36:42 +0530

    airbnb final project

--- vite-react/project-1/App.js
+++ vite-react/project-1/App.js
@@ -1,29 +1,28 @@
 import React from "react"
-import Joke from "./Joke"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
 
 export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                id={item.id}
+                title={it}
+                
+            />
+        )
+    })        
+    
     return (
         <div>
-            <Joke 
-                setup="I got my daughter a fridge for her birthday." 
-                punchline="I can't wait to see her face light up when she opens it." 
-            />
-            <Joke 
-                setup="How did the hacker escape the police?" 
-                punchline="He just ransomware!" 
-            />
-            <Joke 
-                setup="Why don't pirates travel on mountain roads?" 
-                punchline="Scurvy." 
-            />
-            <Joke 
-                setup="Why do bees stay in the hive in the winter?" 
-                punchline="Swarm." 
-            />
-            <Joke 
-                setup="What's the best thing about Switzerland?" 
-                punchline="I don't know, but the flag is a big plus!" 
-            />
+            <Hero />
+            <Navbar />
+            <section className="cards-list">
+                {cards}
+            </section>
         </div>
     )
 }
--- vite-react/project-1/components/Card.js
+++ vite-react/project-1/components/Card.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React from "react"
 
-export default function Card() {
+export default function Card(props) {
+    let badgeText
+    if (props.item.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.item.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    
     return (
         <div className="card">
-            <img src="../images/katie-zaferes.png" className="card--image" />
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`../images/${props.item.coverImg}`} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p className="card--title">{props.item.title}</p>
+            <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
     )
 }
